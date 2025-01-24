86c4941920be01fee2682df202c410c3cb40fc49
0Maan0 <maan.scipio@student.uva.nl>
2025-01-24 10:20:48 +0100

    test corridor fcn

--- test_simulation.py
+++ test_simulation.py
@@ -9,7 +9,4 @@ def test_to_corridor():
     expected_corridor = "#######\nE.....E\nE.....E\nE.....E\nE.....E\nE.....E\n#######"
     assert created_corridor == expected_corridor, f"Expected {expected_corridor}, but got {created_corridor}"
 
-def test_grid():
-    grid = Grid(5, 5)
-    assert grid.grid == [[EMPTY for _ in range(5)] for _ in range(5)], f"Expected {[[EMPTY for _ in range(5)] for _ in range(5)]}, but got {grid.grid}"
-test_to_corridor()
+
