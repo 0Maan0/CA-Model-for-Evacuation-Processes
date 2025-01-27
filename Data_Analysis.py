09414a527e3cad69c114873636691b590cc37b14
sofiatete <sofia.garcia.nunes@gmail.com>
2025-01-27 15:51:15 +0100

    density

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -1,4 +1,4 @@
-from FFCA import FFCA
+from FFCA_wrap import FFCA_wrap
 from Grid import Grid, Pos
 from metrics import order_parameter, mean_order_parameter, plot_order_parameter
 import time
@@ -11,17 +11,15 @@ def lane_formation(rows, cols, Ntot, steps, ffca):
     # Calculate the mean phi for a random distribution of agents
     random_phi_values = []
     for _ in range(500):
-        ffca = FFCA(rows, cols, Ntot)
+        ffca = FFCA_wrap(rows, cols, Ntot)
         N1, N2 = ffca.agents_in_row(ffca.structure)
         random_phi_values.append(order_parameter(Ntot, N1, N2))
     phi_zero = np.mean(random_phi_values)
 
-    ffca = FFCA(rows, cols, Ntot)
+    ffca = FFCA_wrap(rows, cols, Ntot)
     phi_values = np.zeros(steps)
 
     for i in range(steps):
-        time.sleep(0.05)
-        # print(f"Step {i}")
         current_phi = order_parameter(Ntot, *ffca.agents_in_row(ffca.structure))
         current_mean_phi = mean_order_parameter(current_phi, phi_zero)
         phi_values[i] = current_mean_phi
@@ -33,8 +31,8 @@ steps = 1000
 number_agents = 50
 number_rows = 10
 number_cols = 100
-
-ffca = FFCA(number_rows, number_cols, number_agents, spawn_rate=0.025,
+"""
+ffca = FFCA_wrap(number_rows, number_cols, number_agents, spawn_rate=0.025,
                  conflict_resolution_rate=0, alpha=0.3, delta=0.3,
                  static_field_strength=2.5, dynamic_field_strength=3,
                  horizontal_bias=50)  # Adjust parameters as necessary
@@ -48,13 +46,11 @@ phi_values = lane_formation(number_rows, number_cols, number_agents, steps, ffca
 horizontal_bias = [1, 10, 25, 50, 75, 100]
 phi_lists = []
 for bias in horizontal_bias:
-    ffca = FFCA(number_rows, number_cols, number_agents, spawn_rate=0.025,
+    ffca = FFCA_wrap(number_rows, number_cols, number_agents, spawn_rate=0.025,
                  conflict_resolution_rate=0.5, alpha=0.3, delta=0.3,
                  static_field_strength=2.5, dynamic_field_strength=5,
                  horizontal_bias=bias)  
     phi_lists.append(lane_formation(number_rows, number_cols, number_agents, steps, ffca))
-
-print(phi_lists)
 
 # Make a plot with all horizontal bias values (phi values vs iterations)
 
@@ -81,17 +77,17 @@ df = pd.DataFrame(data, columns=columns)
 
 # Save the DataFrame to a CSV file
 df.to_csv("order_parameter_vs_horizontal_bias.csv", index=False)
-
+ """
 
 # Different density values
 total_num_cells = number_rows * number_cols
 
-density_values = [0.1, 0.3, 0.5, 0.7, 0.9]
+density_values = [0.05, 0.1, 0.15, 0.2, 0.25]
 number_agents_list = [int(density * total_num_cells) for density in density_values]
 
-phi_lists = []
+""" phi_lists = []
 for Ntot in number_agents_list:
-    ffca = FFCA(number_rows, number_cols, Ntot, spawn_rate=0.025,
+    ffca = FFCA_wrap(number_rows, number_cols, Ntot, spawn_rate=0.025,
                  conflict_resolution_rate=0, alpha=0.3, delta=0.3,
                  static_field_strength=2.5, dynamic_field_strength=5,
                  horizontal_bias=50)  
@@ -118,15 +114,15 @@ data = np.array(phi_lists).T  # Transpose to align each density as a column
 columns = [f"Density {density}" for density in density_values]
 
 df = pd.DataFrame(data, columns=columns)
-df.to_csv("order_parameter_vs_density.csv", index=False)
+df.to_csv("order_parameter_vs_density.csv", index=False) """
 
 # All densities test
-densities = np.linspace(0.1, 0.9, 100)
+densities = np.linspace(0.05, 0.25, 100)
 phi_values = []
 
 for density in densities:
     Ntot = int(density * total_num_cells)
-    ffca = FFCA(number_rows, number_cols, Ntot, spawn_rate=0.025,
+    ffca = FFCA_wrap(number_rows, number_cols, Ntot, spawn_rate=0.025,
                  conflict_resolution_rate=0, alpha=0.3, delta=0.3,
                  static_field_strength=2.5, dynamic_field_strength=5,
                  horizontal_bias=50)  
@@ -157,7 +153,7 @@ conflict_resolution_rates = [0, 0.25, 0.5, 0.75, 1]
 phi_lists = []
 
 for rate in conflict_resolution_rates:
-    ffca = FFCA(number_rows, number_cols, number_agents, spawn_rate=0.025,
+    ffca = FFCA_wrap(number_rows, number_cols, number_agents, spawn_rate=0.025,
                  conflict_resolution_rate=rate, alpha=0.3, delta=0.3,
                  static_field_strength=2.5, dynamic_field_strength=5,
                  horizontal_bias=50)  
