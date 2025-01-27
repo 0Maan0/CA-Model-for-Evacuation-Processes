0190c8a4d9d2bd3f612f79b5defbcdae6e300b99
0Maan0 <maan.scipio@student.uva.nl>
2025-01-27 16:19:15 +0100

    added congestion metric

--- run_FFCA.py
+++ run_FFCA.py
@@ -77,60 +77,72 @@ def run_wrap():
         ffca.show()
 
 def run_wrap_statistics():
-    cmax = 100
-    rmax = 10
+    cmax = 30
+    rmax = 30
     r = 2
     c = cmax
     left_agents = [(Pos(r, 1 + 2 * i), 1) for i in range(1)]
     right_agents = [(Pos(r, c - 2 * i), 2) for i in range(10)]
     agents = left_agents + right_agents
-    Ntot = 50
-    dynamic_field_strengths = [3.0, 5.0, 7.0, 9.0]
-    for dfs in dynamic_field_strengths:
+    Ntot = 100
+    dfs = 5
+    ffca = FFCA_wrap(rmax, cmax, Ntot, dynamic_field_strength=dfs)
+    steps = 1000
+    ffca.show()
+    random_phi_values = []
+    for _ in range(100):
         ffca = FFCA_wrap(rmax, cmax, Ntot, dynamic_field_strength=dfs)
-        steps = 2000
-        ffca.show()
-        random_phi_values = []
-        for _ in range(100):
-            ffca = FFCA_wrap(rmax, cmax, Ntot, dynamic_field_strength=dfs)
-            N1, N2 = ffca.agents_in_row(ffca.structure)
-            random_phi_values.append(order_parameter(Ntot, N1, N2))
-        phi_zero = np.mean(random_phi_values)
-        ffca = FFCA_wrap(rmax, cmax, Ntot, dynamic_field_strength=dfs)
-        phi_values = np.zeros(steps)
-        flux_values_1 = np.zeros(steps)
-        flux_values_2 = np.zeros(steps)
-        total_flux_counter_1 = 0
-        total_flux_counter_2 = 0
-        for i in range(steps):
-            time.sleep(0.05)
-            # print(f"Step {i}")
-            current_phi = order_parameter(Ntot, *ffca.agents_in_row(ffca.structure))
-            current_mean_phi = mean_order_parameter(current_phi, phi_zero)
+        N1, N2 = ffca.agents_in_row(ffca.structure)
+        random_phi_values.append(order_parameter(Ntot, N1, N2))
+    phi_zero = np.mean(random_phi_values)
+    ffca = FFCA_wrap(rmax, cmax, Ntot, dynamic_field_strength=dfs)
+    phi_values = np.zeros(steps)
+    flux_values_1 = np.zeros(steps)
+    flux_values_2 = np.zeros(steps)
+    total_flux_counter_1 = 0
+    total_flux_counter_2 = 0
+    agent_1_leaving = np.zeros(steps)
+    agent_2_leaving = np.zeros(steps)
+    leave_count1 = 0
+    leave_count2 = 0
+    for i in range(steps):
+        time.sleep(0.05)
+        #print(f"Step {i}")
+        current_phi = order_parameter(Ntot, *ffca.agents_in_row(ffca.structure))
+        current_mean_phi = mean_order_parameter(current_phi, phi_zero)
+        #ffca.show()
+        agent_fluxes = agent_flux(*ffca.agents_at_exit(ffca.structure))
+        total_flux_counter_1 += agent_fluxes[0]
+        total_flux_counter_2 += agent_fluxes[1]
+        flux_values_1[i] = total_flux_counter_1
+        flux_values_2[i] = total_flux_counter_2
+
+        leave_count1 += agent_fluxes[2]
+        leave_count2 += agent_fluxes[3]
+        agent_1_leaving[i] = leave_count1
+        agent_2_leaving[i] = leave_count2
+        phi_values[i] = current_mean_phi
+        ffca.step()
+        if agent_fluxes[2] == 0 or agent_fluxes[3] == 0:
+            print(f"Step {i}")
             ffca.show()
-            agent_fluxes = agent_flux(*ffca.agents_at_exit(ffca.structure))
-            total_flux_counter_1 += agent_fluxes[0]
-            total_flux_counter_2 += agent_fluxes[1]
-            flux_values_1[i] = total_flux_counter_1
-            flux_values_2[i] = total_flux_counter_2
-            phi_values[i] = current_mean_phi
-            ffca.step()
-            ffca.show()
-        # save 2 arrays in csv file
-        np.savetxt(f"simulation_results/flux_values_1_dfs{dfs}.csv", flux_values_1, delimiter=",")
-        np.savetxt(f"simulation_results/flux_values_2_dfs{dfs}.csv", flux_values_2, delimiter=",")
-        # save phi_values in csv file
-        np.savetxt(f"simulation_results/phi_values_dfs{dfs}.csv", phi_values, delimiter=",")
-    #plot_order_parameter(phi_values, steps)
+    # save 2 arrays in csv file
+    np.savetxt(f"simulation_results/flux_values_1_dfs{dfs}.csv", flux_values_1, delimiter=",")
+    np.savetxt(f"simulation_results/flux_values_2_dfs{dfs}.csv", flux_values_2, delimiter=",")
+    np.savetxt(f"simulation_results/agent_1_leaving_dfs{dfs}.csv", agent_1_leaving, delimiter=",")
+    np.savetxt(f"simulation_results/agent_2_leaving_dfs{dfs}.csv", agent_2_leaving, delimiter=",")
+    # save phi_values in csv file
+    np.savetxt(f"simulation_results/phi_values_dfs{dfs}.csv", phi_values, delimiter=",")
+#plot_order_parameter(phi_values, steps)
 
 def run():
     Ntot = 50 # total number of agents
-    steps = 1000 # number of steps in the simulation
-    L = 10 # number of rows
-    W = 100 # number of columns
+    steps = 2000 # number of steps in the simulation
+    L = 30 # number of rows
+    W = 30 # number of columns
     # Calculate the mean phi for a random distribution of agents
     random_phi_values = []
-    for _ in range(100):
+    for _ in range(500):
         ffca = FFCA(L, W, Ntot)
         N1, N2 = ffca.agents_in_row(ffca.structure)
         random_phi_values.append(order_parameter(Ntot, N1, N2))
@@ -203,7 +215,7 @@ def main():
     # test_small()
     # run()
     # debug_wrap()
-    debug_wrap_2()
+    # debug_wrap_2()
     pass
 
 
