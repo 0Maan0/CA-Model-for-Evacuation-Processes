1b835b0228611165e7ec69abdc022f33f6345be8
0Maan0 <maan.scipio@student.uva.nl>
2025-02-02 12:03:11 +0100

    fix plot and add comments

--- metrics.py
+++ metrics.py
@@ -11,6 +11,7 @@ https://journals.aps.org/pre/abstract/10.1103/PhysRevE.75.051402
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 # Order parameter to determine the lane formation in the system
 def order_parameter(Ntot, N1, N2):
@@ -89,7 +90,7 @@ def congestion_index(Ntot, congested_agents):
     """
     return congested_agents / Ntot 
 
-def plot_congestion_and_flux(agent_1_leaving, agent_2_leaving, total_fluxes_agent_1, total_fluxes_agent_2):
+def plot_congestion_and_flux(agent_1_leaving, agent_2_leaving, total_fluxes_agent_1, total_fluxes_agent_2, save_path = "Figures/congestion_flux_subplot.pdf"):
     """
     This function plots the amount of agents leaving at the exits per itaand the total flux of agents in the system.
     """
@@ -111,18 +112,20 @@ def plot_congestion_and_flux(agent_1_leaving, agent_2_leaving, total_fluxes_agen
 
     ax[1].grid(True)
     plt.tight_layout()
-    plt.savefig("Figures/congestion_flux_subplot.pdf")
+    plt.savefig(save_path)
     plt.show()
 
 
-def plot_congestion_flux(densities):
+def plot_congestion_flux(densities, results_path = "simulation_results", save_path = "Figures/total_flux.pdf"):   
+    """
+    This function plots the total flux of agents for varying densities (to show the critical density where congestion happens).
+    """
     flux_values = np.zeros(len(densities))
     std_values = np.zeros(len(densities))
     for i, density in enumerate(densities):
-        if i == 12:
-            print(np.loadtxt(f"simulation_results/congestion_flux_d{density}.csv", delimiter=","))
-        flux_values[i] = np.mean(np.loadtxt(f"simulation_results/congestion_flux_d{density}.csv", delimiter=","))
-        std_values[i] = np.std(np.loadtxt(f"simulation_results/congestion_flux_d{density}.csv", delimiter=","))
+        csv_path = os.path.join(results_path, f"congestion_flux_d{density}.csv")
+        flux_values[i] = np.mean(np.loadtxt(csv_path, delimiter=","))
+        std_values[i] = np.std(np.loadtxt(csv_path, delimiter=","))
     plt.figure(figsize=(10, 6))
     plt.scatter(densities[np.argmax(flux_values)], np.max(flux_values), color='#ef7f36', label=f'Critical density = {np.round(densities[np.argmax(flux_values)],2)}', zorder=2)
     plt.errorbar(densities, flux_values, linestyle='-', marker='o', color='#008083', zorder=1, yerr=std_values)
@@ -130,38 +133,45 @@ def plot_congestion_flux(densities):
     plt.ylabel("Mean Flux", fontsize=14)
     plt.title("Mean flux of agents for varying density", fontsize=16)
     plt.grid(True)
-    plt.xlim([0.01, 0.45])
+    #plt.xlim([0.01, 0.45])
     plt.legend(fontsize=14)
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
     plt.tight_layout()
-    plt.savefig("Figures/total_flux.pdf")
+    plt.savefig(save_path)
     plt.show()
 
-def plot_congestion_index(densities):
+def plot_congestion_index(densities, results_path = "simulation_results", save_path = "Figures/congestion_index.pdf"):
+    """
+    This function plots the congestion index for varying densities (to show the critical density where congestion happens).
+    """
     congestion_values = np.zeros(len(densities))
     congestion_std = np.zeros(len(densities))
+    flux_values = np.zeros(len(densities))
     for i, density in enumerate(densities):
-        congestion_indexes = np.loadtxt(f"simulation_results/congestion_index_d{density}.csv", delimiter=",")
+        csv_path = os.path.join(results_path, f"congestion_index_d{density}.csv")
+        congestion_indexes = np.loadtxt(csv_path, delimiter=",")
         congestion_values[i] = np.mean(congestion_indexes) * 100
         congestion_std[i] = np.std(congestion_indexes) * 100
+        flux_path = os.path.join(results_path, f"congestion_flux_d{density}.csv")
+        flux_values[i] = np.mean(np.loadtxt(flux_path, delimiter=","))
+    critical_index = np.argmax(flux_values)
     plt.figure(figsize=(10, 6))
-    plt.scatter(densities[6], congestion_values[6], color='#ef7f36', label=f'Critical density = {0.21}', zorder=2)
+    plt.scatter(densities[critical_index], congestion_values[critical_index], color='#ef7f36', label=f'Critical density = {np.round(densities[np.argmax(flux_values)],2)}', zorder=2)
     plt.errorbar(densities, congestion_values, linestyle='-', marker='o', color='#008083', zorder=1, yerr=congestion_std)
     plt.xlabel("Density", fontsize=14)
     plt.ylabel("CR (%)", fontsize=14)
     plt.title("Congestion Ratio for varying density", fontsize=16)
     plt.grid(True)
-    plt.xticks(fontsize=12)
-    plt.yticks(fontsize=12)
-    plt.xlim([0.01, 0.45])
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    #plt.xlim([0.01, 0.45])
     plt.legend(fontsize=14)
     plt.tight_layout()
-    plt.savefig("Figures/congestion_index.pdf")
+    plt.savefig(save_path)
     plt.show()
 
 if __name__ == "__main__":
-
     densities = np.linspace(0.05, 0.43, 15)
     plot_congestion_flux(densities)
     plot_congestion_index(densities)
--- run_simulation.py
+++ run_simulation.py
@@ -15,55 +15,73 @@ import numpy as np
 from Visualization import visualize_simulation, grid_to_image
 import imageio
 import cv2
+import os
 
-def run_density_comparison(cmax, rmax, steps, densities):
+def run_density_comparison(cmax, rmax, steps, densities, gif_path, results_path):
+    """
+    This function runs the FFCA simulation for different densities and saves the results in a gif and as a csv file.
+    Input:
+    cmax: amount of columns in the grid (Int)
+    rmax: amount of rows in the grid (Int)
+    steps: amount of steps the simulation will run (Int)
+    densities: array of densities to run the simulation for (Array)
+    """
+    # Create directory for results, if it does not exist yet
+    if not os.path.exists(results_path):
+        os.makedirs(results_path)
+    if not os.path.exists(gif_path):
+        os.makedirs(gif_path)
+
     for density in densities:
         Ntot_half = int(density * cmax * rmax//2)
         Ntot = Ntot_half * 2
-        ffca = FFCA_wrap(rmax, cmax, Ntot_half, spawn_rate=0.025,
-                        conflict_resolution_rate=0, alpha=0.3, delta=0.3,
-                        static_field_strength=2.5, dynamic_field_strength=3,
-                        horizontal_bias=5000)
 
+        # Determine Phi0
         random_phi_values = []
         for _ in range(100):
             ffca = FFCA_wrap(rmax, cmax, Ntot_half, spawn_rate=0.025,
                         conflict_resolution_rate=0, alpha=0.3, delta=0.3,
-                        static_field_strength=2.5, dynamic_field_strength=3,
+                        static_field_strength=2.5, dynamic_field_strength=0,
                         horizontal_bias=5000)
             N1, N2 = ffca.agents_in_row(ffca.structure)
             random_phi_values.append(order_parameter(Ntot, N1, N2))
         phi_zero = np.mean(random_phi_values)
         ffca = FFCA_wrap(rmax, cmax, Ntot_half, spawn_rate=0.025,
                         conflict_resolution_rate=0, alpha=0.3, delta=0.3,
-                        static_field_strength=2.5, dynamic_field_strength=3,
+                        static_field_strength=2.5, dynamic_field_strength=0,
                         horizontal_bias=5000)
         phi_values = np.zeros(steps)
         congestion_indexes = np.zeros(steps)
         congestion_fluxes = np.zeros(steps)
 
+        # Start simulation
         frames = []
         for i in range(steps):
             img = grid_to_image(ffca.structure)
             frames.append(img)
             current_phi = order_parameter(Ntot_half, *ffca.agents_in_row(ffca.structure))
             current_mean_phi = mean_order_parameter(current_phi, phi_zero)
-            ffca.step()
-            agent_fluxes = agent_flow(*ffca.agents_at_exit())
-
             phi_values[i] = current_mean_phi
+
+            ffca.step()
+
             congested_agents = Ntot - ffca.get_amount_agents_moved_forward()
             congestion_indexes[i] = congestion_index(Ntot, congested_agents)
+
             net_flow = ffca.global_movement()
             congestion_fluxes[i] = flux(net_flow, Ntot, cmax, rmax, steps)
-        filename = f"gifs/gif{density}.gif"
-        imageio.mimsave(filename, frames)
-        print(f"GIF saved as {filename}")
-        # save 2 arrays in csv file
-        np.savetxt(f"simulation_results/congestion_flux_d{density}.csv", congestion_fluxes, delimiter = ",")
-        np.savetxt(f"simulation_results/congestion_index_d{density}.csv", congestion_indexes, delimiter=",")
-        # save phi_values in csv file
-        np.savetxt(f"simulation_results/phi_values_d{density}.csv", phi_values, delimiter=",")
+
+        # Save gif and csv files
+        filename_gif = os.path.join(gif_path, f"gif{density}.gif")
+        imageio.mimsave(filename_gif, frames)
+
+        congestion_flux_csv = os.path.join(results_path, f"congestion_flux_d{density}.csv")
+        congestion_index_csv = os.path.join(results_path, f"congestion_index_d{density}.csv")
+        phi_values_csv = os.path.join(results_path, f"phi_values_d{density}.csv")
+
+        np.savetxt(congestion_flux_csv, congestion_fluxes, delimiter=",")
+        np.savetxt(congestion_index_csv, congestion_indexes, delimiter=",")
+        np.savetxt(phi_values_csv, phi_values, delimiter=",")
 
 
 
@@ -73,9 +91,9 @@ def main():
     cmax = 50
     rmax = 25
     steps = 1000
-    run_density_comparison(cmax, rmax, steps, densities)
-    pass
-
+    gif_path = "gifs"
+    results_path = "simulation_results"
+    run_density_comparison(cmax, rmax, steps, densities, gif_path, results_path)
 
 if __name__ == "__main__":
     main()
